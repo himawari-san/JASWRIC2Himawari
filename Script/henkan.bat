074db0563da8ddd3f1e1bd31a4a737438c2b446d
himawari-san <himawari@ninjal.ac.jp>
2023-03-13 14:47:01 +0900

    Update for JASWRIC ver.1.1

--- Script/henkan.bat
+++ Script/henkan.bat
@@ -1,5 +1,5 @@
 echo off
 echo テキスト変換を開始します。少しお待ちください。
-perl JASWRIC2Himawari.pl "JASWRIC_Participant Survey.txt" JASWRIC_Tagged.txt Scanned > ../Package/Corpora/JASWRIC/corpus.xml
+perl JASWRIC2Himawari.pl "JASWRIC_Participant Survey.txt" JASWRIC_Tagged_v1.1.txt Scanned > ../Package/Corpora/JASWRIC/corpus.xml
 echo 処理が終了しました。エラーが表示されていないか，確認してください。
 pause 
