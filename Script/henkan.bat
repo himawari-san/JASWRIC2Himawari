308cb32bff2d0f7942e46bb5b199b3ca63f4b272
himawari-san <himawari@ninjal.ac.jp>
2023-03-13 11:07:53 +0900

    Use 'JASWRIC_Participant Survey.txt' as the input file of JASWRIC2Himawari.pl

--- Script/henkan.bat
+++ Script/henkan.bat
@@ -1,6 +1,5 @@
 echo off
-echo テキスト変換を開始します。
-echo perl JASWRIC2Himawari.pl JASWRIC_Participant_Survey.txt JASWRIC_Tagged.txt Scanned ^> corpus.xml
-perl JASWRIC2Himawari.pl JASWRIC_Participant_Survey.txt JASWRIC_Tagged.txt Scanned > corpus.xml
-echo 処理が終了しました。
+echo テキスト変換を開始します。少しお待ちください。
+perl JASWRIC2Himawari.pl "JASWRIC_Participant Survey.txt" JASWRIC_Tagged.txt Scanned > ../Package/Corpora/JASWRIC/corpus.xml
+echo 処理が終了しました。エラーが表示されていないか，確認してください。
 pause 
